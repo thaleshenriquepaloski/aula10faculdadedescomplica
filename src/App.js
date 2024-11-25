7a969bff8f556686992028871809aadeb61adaab
Thales Henrique Paloski <thalespaloski@gmail.com>
2024-11-25 14:52:43 -0300

    Primeiro Commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import Header from "./components/header";
+import ListarTarefa from "./pages/tarefa/ListarTarefa";
+import "./App.css";
+
+function App() {
+
+  return (
+    <div className="App">
+        <Header />
+        <ListarTarefa />
+    </div>
+  )
+};
+
+export default App
